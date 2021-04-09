94664ab833f4b3f4fd923fb0313fe958bbf7385b
michielpost <michiel@q42.nl>
2021-04-09 19:40:28 +0200

    Added spotlight

--- WebApp/wwwroot/js/color.js
+++ WebApp/wwwroot/js/color.js
@@ -5,6 +5,8 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/colorHub").build()
 connection.on("ReceiveMessage", function (color) {
     document.getElementById('overlay').style.background = color;
     document.getElementById('current').style.background = color;
+    document.getElementById('t1').style['border-color'] = color + ' transparent transparent transparent';
+    document.getElementById('t2').style['border-color'] = color + ' transparent transparent transparent';
 });
 
 connection.start().then(function () {
